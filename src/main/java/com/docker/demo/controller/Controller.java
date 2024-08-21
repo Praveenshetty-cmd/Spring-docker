ac391e989dae8212e17acef182897f877f62ef52
itd-rahulpalakurthi <157454465+itd-rahulpalakurthi@users.noreply.github.com>
2024-08-21 18:06:39 +0530

    first commit

---
+++ src/main/java/com/docker/demo/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.docker.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // Optional: to group endpoints under /api
+public class Controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test method";
+    }
+}
